2fce2e03e9e33a54f9abe54fdddf518fb5aac6ca
athos777x <159774085+athos777x@users.noreply.github.com>
2025-06-22 21:46:38 +0800

    Remove unused math import from settings page to clean up code and improve maintainability.

--- lib/settingspage.dart
+++ lib/settingspage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:math' as math;
 import 'onboarding_screen.dart';
 import 'services/medication_service.dart';
 import '../healthpage.dart';
